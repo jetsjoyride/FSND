4a8939f69f456af40e52665ad2f36bfb034d5791
Jason-Siegel <jason.siegel@pw.utc.com>
2020-08-09 09:15:37 -0400

    Initial Search Term implementation - more to do

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -267,18 +267,20 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
+    # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get('search_term','')
+    results = Artist.query.filter(Artist.name.ilike('%'+search_term.lower()+'%'))
+    data = []
+    # Jason To Do: need query for num_upcoming_shows
+    for result in results:
+        data += [{'id':result.id,'name':result.name,'num_upcoming_shows':0}]
+    response={
+        "count": results.count(),
+        "data": data
+    }
+    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
